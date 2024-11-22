86e8a10114284787184653210dd7a59e87d55d26
VJD357 <vijay.mohan.dhanwant@gmail.com>
2024-11-22 11:16:59 +0530

    setup2

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
    #!/bin/bash
-   sudo apt-get update -y
-   sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
+   echo "setup.sh started" >> setup.log
+   sudo apt-get update -y >> setup.log 2>&1
+   sudo apt-get install -y tesseract-ocr tesseract-ocr-eng >> setup.log 2>&1
+   echo "setup.sh finished" >> setup.log
